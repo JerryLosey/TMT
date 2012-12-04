4e6b4683af1bb5d7b2d4bc88f6a4b997eba3f476
loseyjerry <jlosey90@gmail.com>
2012-12-03 21:49:14 -0500

    Cancel break/damage/decay events

---
+++ src/me/turkey2349/plugin/BlockBreakListener.java
@@ -0,0 +1,40 @@
+package me.turkey2349.plugin;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.LeavesDecayEvent;
+
+public class BlockBreakListener implements Listener {
+	
+	private TMT plugin;
+
+	public BlockBreakListener(TMT plugin){
+		this.plugin = plugin;
+	}
+
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent e)
+	{
+		e.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onBlockDamage(BlockDamageEvent e)
+	{
+		e.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onLeavesDecay(LeavesDecayEvent e)
+	{
+		e.setCancelled(true);
+	}
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent e)
+	{
+		e.setCancelled(true);
+	}
+}
