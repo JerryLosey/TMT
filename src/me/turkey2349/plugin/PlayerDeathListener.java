c34baffdbde2a336b093a3ed301034f08f78bbaf
Turkey2349 <turkey2349@gmail.com>
2013-01-28 21:53:48 -0500

    Update src/me/turkey2349/plugin/PlayerDeathListener.java

--- src/me/turkey2349/plugin/PlayerDeathListener.java
+++ src/me/turkey2349/plugin/PlayerDeathListener.java
@@ -1,78 +1,113 @@
 package me.turkey2349.plugin;
 
+import java.util.Collection;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+
 public class PlayerDeathListener implements Listener {
 	
 	TMT plugin;
-	TMT JoinedPlayers;
-	TMT InnocentPlayers;
-	TMT TraitorPlayers;
-	TMT DeadPlayers;
-	TMT DetectivePlayers;
+	TMT Joinedplayers;
+	TMT Innocentplayers;
+	TMT Traitorplayers;
+	TMT Deadplayers;
+	TMT Detectiveplayers;
+	TMT players;
 	
-	public PlayerDeathListener(TMT plugin){
+	public PlayerDeathListener(TMT plugin)
+	{
 		this.plugin = plugin;
 	}
 
+	@SuppressWarnings("unchecked")
 	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent deathevent)
+	public void onplayerNameDeath(PlayerDeathEvent deathevent)
 	{
-		Player player = deathevent.getEntity();
-	//	String name = player.getDisplayName();
+		String playerName = deathevent.getEntity().getDisplayName();
+		//String killer = deathevent.getEntity().getKiller().getDisplayName();
+		
+		deathevent.setDeathMessage("" + playerName + " Died");
+		
 		
 		Location location = deathevent.getEntity().getLocation();
 		location.getBlock().setType(Material.SIGN_POST);
 		Block signBlock = location.getBlock();
-		if(signBlock.getType() == Material.SIGN_POST)
-		{
+		if(signBlock.getType() == Material.SIGN_POST){
 			plugin.getServer().broadcastMessage(""+ signBlock.getState()+"");
 			Sign sign = (Sign) signBlock.getState();
-			sign.setLine(1, ChatColor.GREEN + player.getDisplayName());
-			sign.setLine(2, ChatColor.UNDERLINE + player.getDisplayName());
-			
+			sign.setLine(1, ChatColor.GREEN + playerName);
+			// Detect if traitor or innocent
+			String Assign = TMT.players.get(playerName);
+			sign.setLine(2, ChatColor.UNDERLINE + Assign);
 			sign.update(true);
 		}
-		/* if(InnocentPlayers.contains(name)
+		if(Innocentplayers.Icontains(playerName))
 		{
-		    InnocentPlayers.Iremove(name);
-		    Players.remove(name);
-		    DeadPlayers.Dadd(name);
+		    Innocentplayers.Iremove(playerName);
+		    TMT.players.remove(playerName);
+		    TMT.DeadPlayers.add(playerName);
 		}
-		if(triatorPlayers.contians(name))
+		if(TMT.TraitorPlayers.contains(playerName))
 		{
-		    TriatorPlayers.Tremove(name);
-		    Players.remove(name);
-		    DeadPlayers.Dadd(name);
+		    TMT.TraitorPlayers.remove(playerName);
+		    TMT.players.remove(playerName);
+		    TMT.DeadPlayers.add(playerName);
 		}
-		if(InnocentPlayers.size == 0)
+		if(TMT.InnocentPlayers.size() == 0)
 		{
-		    getServer.BroadcastMessage("The Traitors have Won!");
-		    -Tp All players back to Spawn
-		    TriatorPlayers.moveall(/*to Joined Players*//*);
-		    InnoccentPlayers.moveall(/*to Joined Players*//*);
-		    DeadPlayers.moveall(/*to Joined Players*//*);
-		    Players.clear();
+		    plugin.getServer().broadcastMessage("The Traitors have Won!");
+		    TMT.JoinedPlayers.addAll((Collection<? extends String>) Deadplayers);
+		    TMT.JoinedPlayers.addAll((Collection<? extends String>) Traitorplayers);
+		    TMT.JoinedPlayers.addAll((Collection<? extends String>) Innocentplayers);
+		    for(int c = 0; TMT.JoinedPlayers.size() >= c; c++)
+		    {
+		    	String TpPlayer = TMT.JoinedPlayers.get(c);
+		    	Player player = Bukkit.getPlayer(TpPlayer);
+		    	//player.teleport((Location) plugin.getcustomConfig().get("Spawn"));
+		    }
+		    TMT.players.clear();
 		    
 		}
-		if(TraitorPlayers.size == 0)
+		if(TMT.TraitorPlayers.size() == 0)
 		{
-		    getServer.BroadcastMessage("The Innocent have Won!");
-		    -Tp All players back to Spawn
-		    TriatorPlayers.moveall(/*to Joined Players*//*);
-		    InnoccentPlayers.moveall(/*to Joined Players*//*);
-		    DeadPlayers.moveall(/*to Joined Players*//*);
-		    Players.clear();
+			plugin.getServer().broadcastMessage("The Innocent have Won!");
+			 TMT.JoinedPlayers.addAll((Collection<? extends String>) Deadplayers);
+			    TMT.JoinedPlayers.addAll((Collection<? extends String>) Traitorplayers);
+			    TMT.JoinedPlayers.addAll((Collection<? extends String>) Innocentplayers);
+			    for(int c = 0; TMT.JoinedPlayers.size() >= c; c++)
+			    {
+			    	String TpPlayer = TMT.JoinedPlayers.get(c);
+			    	Player player = Bukkit.getPlayer(TpPlayer);
+			    //	player.teleport((Location) plugin.getcustomConfig().get("Spawn"));
+			    }
+			    TMT.players.clear();
 		    
-		}*/
+		}
+
+		
+		//if(InnocentplayerNames.Icontains(playerName))
+		//{
+			//TMT.setLine(1, "Innocent");
+		//}
+		//else if(TraitorplayerNames.Tcontains(playerName))
+		//{
+		//	TMT.setLine(1, "Traitor");
+		//}
+		
+		//DeadplayerNames.Dadd(playerName);
+		//TraitorplayerNames.Tremove(playerName);
+		//InnocentplayerNames.Iremove(playerName);
+
+		
 	}
 }
